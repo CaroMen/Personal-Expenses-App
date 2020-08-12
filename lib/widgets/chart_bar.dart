4e32ab6b064fad96f5a5488669247255bd72c5c9
Caroline <caroline_mendez@baylor.edu>
2020-08-12 17:49:18 -0500

    chart and new cards

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,13 +11,18 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
+        ),
         SizedBox(
           height: 4,
         ),
         Container(
           height: 60,
-          width: 10,
+          width: 20,
           child: Stack(
             children: <Widget>[
               Container(
